250433fb6c4bbaa618adc766eddb97f7adc9b07c
nv08 <41137770+nv08@users.noreply.github.com>
2021-07-09 06:59:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,23 +4,12 @@ const logger = require('./logConfiguration')
 
 function startServer({serverPort = process.env.SERVER_PORT}={}){
 
-    try{
-        
-        app.listen(process.env.PORT,'0.0.0.0',()=>
-        {
-            logger.info({
-                level:'info',
-                message:`Server is being initialized on ${process.env.PORT}`,
-                
-            })
-            console.log(`Server is being Initialized on ${serverPort}!`)
-            console.log(`Server Started on ${serverPort}`);
-        })
+       try{
+
+        app.listen(process.env.PORT,0.0.0.0)
     }
     catch(err)
     {
-        throw err
-    }
 }
 
 module.exports = {startServer,app}
